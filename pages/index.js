cafa9ca50c4673502514ff5e3721fc488eacc2bd
gajen <gajen007@gmail.com>
2022-12-15 13:58:25 -0500

    usedDummyTemplate

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.css'
 import { Provider } from 'react-redux'
 import store from '../store/store'
 import Dashboard from './Dashboard'
+import Calculator from '../components/Calculator';
 export default function Home() {
   return (<Provider store={store}><Dashboard /></Provider>)
 }
